c35b12b9cb997240b41abbd389c37bb545bd2348
Aleem Awan <aleemawan366@outlook.com>
2022-05-16 13:00:23 +0500

    adds annotations in sleep night table

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,9 +15,23 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sleep_night_table")
 data class SleepNight  (
+
+    @PrimaryKey(autoGenerate = true)
     var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_ms")
     val startTimeMilli: Long =  System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_ms")
     var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
     var sleepQuality: Int = -1
 )
